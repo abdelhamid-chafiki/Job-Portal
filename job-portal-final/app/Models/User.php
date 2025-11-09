6452da60e36274cc074eb4e774359a981ebe642b
Abdelhamid <abdelhamidchafiki@gmail.com>
2025-11-09 14:40:49 +0100

    Phase 1: Database, Models, and Relations complete

--- job-portal-final/app/Models/User.php
+++ job-portal-final/app/Models/User.php
@@ -6,9 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    //
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
@@ -17,10 +19,12 @@ class User extends Authenticatable
      *
      * @var list<string>
      */
+    //
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role',
     ];
 
     /**
@@ -44,5 +48,14 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+    public function jobs()
+    {
+        return $this->hasMany(Job::class);
+    }
+
+    public function applications()
+    {
+        return $this->hasMany(Application::class);
     }
 }
